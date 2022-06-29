34d10e25db6d11e1e5cc1581a6e92f905e546b3a
erik <erik@iotech.systems>
2022-06-29 16:40:59 +0200

    added file watcher

--- datatypes/modbusGPIO.py
+++ datatypes/modbusGPIO.py
@@ -8,11 +8,12 @@ class modbusGPIO(object):
 
    def __init__(self, elmt: et.Element):
       self.id: int = int(elmt.attrib["id"])
+      self.lbl: str = elmt.attrib["lbl"]
       self.enabled: bool = (elmt.attrib["enabled"] in ["on", "ON", "1"])
       self.on: str = elmt.attrib["on"]
       self.off: str = elmt.attrib["off"]
       self.note: str = elmt.attrib["note"]
 
    def __str__(self):
-      return f"\t[ gpio enabled: {self.enabled} | id: {self.id} | " \
-         f"on: {self.on} | off: {self.off} | note: {self.note} ]"
+      return f"\t[ gpio enabled: {self.enabled} | id: {self.id} | lbl: {self.lbl} |" \
+         f" on: {self.on} | off: {self.off} | note: {self.note} ]"
