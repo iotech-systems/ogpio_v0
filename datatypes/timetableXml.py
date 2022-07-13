61a24242fd3395f8063c8b63b116ee299e06d9c8
erik <erik@iotech.systems>
2022-07-13 10:58:04 +0200

    added timetime dump

--- datatypes/timetableXml.py
+++ datatypes/timetableXml.py
@@ -29,18 +29,21 @@ class timetableXml(object):
       LOC_INFO: locationTxtInfo = locationTxtInfo("location.txt")
       LOC_INFO.load()
       sclk = sunClock(loc_info=LOC_INFO)
-      # -- --
+      # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+      # <gpio enabled="on" id="0" lbl="CH1" on="sunset" off="sunrise" note="kasetony" />
       arr = [f"\n\t{sclk}"]
       for gpio in gpios:
-         # <gpio enabled="on" id="0" lbl="CH1" on="sunset" off="sunrise" note="kasetony" />
          enb = "{:3s}".format(gpio.attrib["enabled"]); id = gpio.attrib["id"]
          lbl = gpio.attrib["lbl"]; ton = gpio.attrib["on"]
          toff = gpio.attrib["off"]; note = gpio.attrib["note"]
          dton = sclk.get_time_v1(ton); dtoff = sclk.get_time_v1(toff)
+         _on = "{:20s}".format(f"{ton}/{dton}")
+         _off = "{:20s}".format(f"{toff}/{dtoff}")
          state = "1" if (clock.get_state(dton, dtoff)) else "0"
          _msg = f"\n\tGPIO [ enabled: {enb} | id: {id} | calc_state: {state} |" \
-            f" lbl: {lbl} | on: {ton} / {dton} | off: {toff} / {dtoff} ]"
+            f" lbl: {lbl} | on: {_on} | off: {_off} ]"
          arr.append(_msg)
+      # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
       # -- print buffer --
       tbl = "".join(arr)
       return f"\n\t- - - [ timetable ] - - -\n\tfile: {self.path}\n{tbl}\n\n"
